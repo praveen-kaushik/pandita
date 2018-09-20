a248bf0a812720cc31ea40598eb90323cc61260a
Praveen Kaushik <praveenkaushik80@gmail.com>
2018-09-20 16:13:46 +0530

    package

---
+++ pandita/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    repo
+
+    :copyright: (c) 2018 by PraveenKaushik.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'pandita'
+__version__ = '0.0.1'
+__author__ = 'PraveenKaushik'
